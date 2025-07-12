50295c1cbfd1a142c03996eb3cc5d9030b76e82c
snd1234 <sandeepkp23@gmail.com>
2025-07-12 11:38:02 +0530

    Assignment 1 files

---
+++ task1.py
@@ -0,0 +1,17 @@
+first_number = int(input('Enter first number : ' ))
+second_number = int(input('Enter second number : '))
+sum = first_number + second_number
+substraction = first_number - second_number
+product = first_number * second_number
+division = first_number / second_number
+quotient = first_number // second_number
+remainder = first_number % second_number
+
+print('Addition : ',sum)
+print('Substraction : ',substraction)
+print('Product : ',product)
+print('Division : ',division)
+print('Quotient : ',quotient)
+print('Remainder : ',remainder)
+
+
---
+++ task2.py
@@ -0,0 +1,5 @@
+first_name = input('Enter your first name : ')
+last_name = input('Enter your last name : ')
+full_name = first_name + ' ' + last_name
+final_output = 'Hello, ' + full_name + ' Welcome to Python program.'
+print(final_output)
